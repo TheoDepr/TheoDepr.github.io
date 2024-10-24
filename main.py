e7e38022148078d129893be0c2051f17dcf2f997
thedprae <Theo.Depraetere@UGent.be>
2024-10-24 15:18:57 +0200

    update

--- main.py
+++ main.py
@@ -3,34 +3,32 @@ from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
+def artwork(i):
+    st.divider()
+    st.image("images/" + str(i) + ".jpg")
+    st.write("Artwork " + str(i))
+    st.divider()
+    return 
+
 
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Jubilee",
+        page_icon="💎",
     )
+ 
+    st.write("# Jubilee ")
+    st.write("## Ron Mueck")
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    st.write("### Zaal 1")
+    artwork(1)
+    st.write("### Zaal 2")
+    st.write("### Zaal 3")
+    st.write("### Zaal 4")
+    st.write("### Zaal 5")
+    st.write("### Zaal 6")
+    st.write("### Zaal 7")
+    st.write("### Zaal 8")
 
 
 if __name__ == "__main__":
